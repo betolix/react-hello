951e56fadf3927ef9f2aa884db53777f75310add
betolix <betolix@gmail.com>
2022-11-05 01:10:14 +0000

    blixs commit

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const SecondsCounter  = (props) => {
+	return (
+		<div className="conatiner">
+			<div className="counter">
+			<p>{props.numSix}</p>
+			<p>{props.numFive}</p>
+			<p>{props.numFour}</p>
+			<p>{props.numThree}</p>
+			<p>{props.numTwo}</p>
+			<p>{props.numOne}</p>
+
+			</div>
+		</div>
+	);
+};
+
+SecondsCounter.propTypes = {
+	numOne: PropTypes.number,
+	numTwo: PropTypes.number,
+	numThree: PropTypes.number,
+	numFour: PropTypes.number,
+	numFive: PropTypes.number,
+	numSix: PropTypes.number,
+	
+}
+
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,24 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import SecondsCounter from "./component/SecondsCounter.jsx";
+
+let count =0;
+setInterval(() => {
+    const numberOne = Math.floor( count / 1) % 10;
+    const numberTwo = Math.floor( count / 10) % 10;
+    const numberThree = Math.floor( count / 100) % 10;
+    const numberFour = Math.floor( count / 1000) % 10;
+    const numberFive = Math.floor( count / 10000) % 10;
+    const numberSix = Math.floor( count / 100000) % 10;
+
+    count ++;
+
+
+    ReactDOM.render(<SecondsCounter numOne={numberOne} numTwo={numberTwo} numThree={numberThree} numFour={numberFour}  numFive={numberFive}  numSix={numberSix} />, document.querySelector("#app"));
+    //ReactDOM.render(<SecondsCounter numTwo={numberTwo} />, document.querySelector("#app"));
+
+  }, 0.00000001);
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
